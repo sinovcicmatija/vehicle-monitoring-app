b38cfe660eea0b8a6b84475b965c74596adfc63e
sinovcicmatija <sinovcicmatija@gmail.com>
2025-07-02 10:13:42 +0200

    Finished registration and login logic on backend

---
+++ backend/vehicle-api/vehicle-api/Models/DTO/LoginUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace vehicle_api.Models.DTO
+{
+    public class LoginUserDTO
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
